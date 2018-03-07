666d58722901c3cc5f3b90f087e0907659402a81
Meghna <meghnanag16@gmail.com>
2018-03-06 21:00:53 -0800

    Initial js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var searchTerm =$(this).attr('q')
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+queryURL=queryURL+ '?' + $.param({
+  'api-key': "86fde0a648424cc3a58e7f5147509bb3" ,
+  'q': searchTerm
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
